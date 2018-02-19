a2de5f1b7a5582b0f1f42ac74c08f96437649096
Jonathan Cespedes <cespedesjonathan25@gmail.com>
2018-02-19 14:39:37 -0400

    create and edit ready

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,16 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Todo;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+
 
 class DefaultController extends Controller
 {
@@ -13,9 +23,118 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+      $todo = $this
+      ->getDoctrine()
+      ->getRepository('AppBundle:Todo')
+      ->findAll();
+
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/home.html.twig', array('todo' => $todo));
+    }
+
+    /**
+    * @Route("/create", name="crear")
+    */
+    public function createAction(Request $request){
+      $todo = new Todo();
+
+      $form = $this->CreateFormBuilder($todo)
+      ->add('titulo', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin:25px')))
+      ->add('enlace', UrlType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin:25px')))
+      ->add('prioridad', ChoiceType::class, array('attr'=>array('class'=>'nav-link dropdown-toggle', 'style' => 'margin:10px'),
+        'choices'  => array(
+        'Alto' => 'Alto',
+        'Normal' => 'Normal',
+        'Bajo' => 'Bajo'),))
+      ->add('estado', ChoiceType::class, array('attr'=>array('class'=>'nav-link dropdown-toggle', 'style' => 'margin:10px'),
+        'choices'  => array(
+        'Por hacer' => 'Por hacer',
+        'Haciendo' => 'Haciendo',
+        'Hecho' => 'Hecho'),))
+      ->add('fechaEntrega', DateTimeType::class, array('attr'=>array('class'=>'formcontrol', 'style'=>'margin-bottom:15px')))
+      ->add('Guardar', SubmitType::class, array('attr'=>array('class'=>'btn btn-primary', 'style' => 'margin:15px')))
+      ->getForm();
+
+      $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+          $title = $form['titulo']->getData();
+          $enlace = $form['enlace']->getData();
+          $estado = $form['estado']->getData();
+          $prioridad = $form['prioridad']->getData();
+          $fechaEntrega = $form['fechaEntrega']->getData();
+
+          $todo->setTitulo($title);
+          $todo->setEnlace($enlace);
+          $todo->setEstado($estado);
+          $todo->setPrioridad($prioridad);
+          $todo->setFechaEntrega($fechaEntrega);
+
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($todo);
+          $em->flush();
+          $this->addFlash(
+            'notice', 'Actividad creada'
+          );
+          return $this->redirectToRoute('homepage');
+      }
+
+      return $this->render('crud/crear.html.twig', array('form' => $form->createView(), 'todo' => $todo));
+    }
+
+    /**
+    * @Route("/edit/{id}", name="editar")
+    */
+
+    public function editAction($id, Request $request){
+      $todo = $this->getDoctrine()
+       ->getRepository('AppBundle:Todo')
+       ->find($id);
+
+       $form = $this->CreateFormBuilder($todo)
+       ->add('titulo', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin:25px')))
+       ->add('enlace', UrlType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin:25px')))
+       ->add('prioridad', ChoiceType::class, array('attr'=>array('class'=>'nav-link dropdown-toggle', 'style' => 'margin:10px'),
+         'choices'  => array(
+         'Alto' => 'Alto',
+         'Normal' => 'Normal',
+         'Bajo' => 'Bajo'),))
+       ->add('estado', ChoiceType::class, array('attr'=>array('class'=>'nav-link dropdown-toggle', 'style' => 'margin:10px'),
+         'choices'  => array(
+         'Por hacer' => 'Por hacer',
+         'Haciendo' => 'Haciendo',
+         'Hecho' => 'Hecho'),))
+       ->add('fechaEntrega', DateTimeType::class, array('attr'=>array('class'=>'formcontrol', 'style'=>'margin-bottom:15px')))
+       ->add('Guardar', SubmitType::class, array('attr'=>array('class'=>'btn btn-primary', 'style' => 'margin:15px')))
+       ->getForm();
+
+       $form->handleRequest($request);
+
+       if ($form->isSubmitted() && $form->isValid()) {
+         $title = $form['titulo']->getData();
+         $enlace = $form['enlace']->getData();
+         $estado = $form['estado']->getData();
+         $prioridad = $form['prioridad']->getData();
+         $fechaEntrega = $form['fechaEntrega']->getData();
+
+         $todo->setTitulo($title);
+         $todo->setEnlace($enlace);
+         $todo->setEstado($estado);
+         $todo->setPrioridad($prioridad);
+         $todo->setFechaEntrega($fechaEntrega);
+
+         $em = $this->getDoctrine()->getManager();
+         $em->persist($todo);
+         $em->flush();
+         $this->addFlash(
+           'notice', 'Actividad editada'
+         );
+         return $this->redirectToRoute('homepage');
+       }
+
+     return $this->render('crud/editar.html.twig', array(
+       'todo' => $todo,
+       'form' => $form->createView(),
+     ));
     }
 }
---
+++ src/AppBundle/Entity/Todo.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Todo
+ *
+ * @ORM\Table(name="todo")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TodoRepository")
+ */
+class Todo
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titulo", type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="enlace", type="string", length=255)
+     */
+    private $enlace;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="estado", type="string", length=255)
+     */
+    private $estado;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prioridad", type="string", length=255)
+     */
+    private $prioridad;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_inicio", type="datetime")
+     */
+    private $fechaInicio;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_entrega", type="datetime")
+     */
+    private $fechaEntrega;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     *
+     * @return Todo
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set enlace
+     *
+     * @param string $enlace
+     *
+     * @return Todo
+     */
+    public function setEnlace($enlace)
+    {
+        $this->enlace = $enlace;
+
+        return $this;
+    }
+
+    /**
+     * Get enlace
+     *
+     * @return string
+     */
+    public function getEnlace()
+    {
+        return $this->enlace;
+    }
+
+    /**
+     * Set estado
+     *
+     * @param string $estado
+     *
+     * @return Todo
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    /**
+     * Get estado
+     *
+     * @return string
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * Set prioridad
+     *
+     * @param string $prioridad
+     *
+     * @return Todo
+     */
+    public function setPrioridad($prioridad)
+    {
+        $this->prioridad = $prioridad;
+
+        return $this;
+    }
+
+    /**
+     * Get prioridad
+     *
+     * @return string
+     */
+    public function getPrioridad()
+    {
+        return $this->prioridad;
+    }
+
+    /**
+     * Set fechaInicio
+     *
+     * @param \DateTime $fechaInicio
+     *
+     * @return Todo
+     */
+    public function setFechaInicio($fechaInicio)
+    {
+        $this->fechaInicio = $fechaInicio;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaInicio
+     *
+     * @return \DateTime
+     */
+    public function getFechaInicio()
+    {
+        return $this->fechaInicio;
+    }
+
+    /**
+     * Set fechaEntrega
+     *
+     * @param \DateTime $fechaEntrega
+     *
+     * @return Todo
+     */
+    public function setFechaEntrega($fechaEntrega)
+    {
+        $this->fechaEntrega = $fechaEntrega;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaEntrega
+     *
+     * @return \DateTime
+     */
+    public function getFechaEntrega()
+    {
+        return $this->fechaEntrega;
+    }
+}
+
